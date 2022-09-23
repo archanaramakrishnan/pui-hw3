ed0cd59951775f9996d6b834e4ff16b7e1fae857
Archana Ramakrishnan <archana2359@gmail.com>
2022-09-22 22:19:11 -0400

    Product grid fixed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import original from './products/original-cinnamon-roll.jpg';
+import apple from './products/apple-cinnamon-roll.jpg';
+import raisin from './products/raisin-cinnamon-roll.jpg';
+import walnut from './products/walnut-cinnamon-roll.jpg';
+import chocolate from './products/double-chocolate-cinnamon-roll.jpg';
+import strawberry from './products/strawberry-cinnamon-roll.jpg';
+import logo from './logo/logo-01.svg';
+
+import Roll from './Roll.js';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <img className="logo" src={logo} alt="logo of the bun bun bake shop which is a doodle of a cinammon roll"/>
+        <div className="navbar">
+          <a className="navbar-products">PRODUCTS</a>
+          <b className="navbar-cart">CART</b>
+          <hr/>
+          <p className="tagline">Our hand-made cinnamon rolls</p>
+        </div>
       </header>
+
+      <div className="list">
+        <Roll 
+          imageURL={original} 
+          rollName="Original cinammon roll"
+          price="2.49" />
+        <Roll 
+          imageURL={apple} 
+          rollName="Apple cinammon roll"
+          price="3.49" />
+        <Roll 
+          imageURL={raisin} 
+          rollName="Raisin cinammon roll"
+          price="2.99" />
+      </div>
+
+      <div className="list">
+        <Roll 
+          imageURL={walnut} 
+          rollName="Walnut cinammon roll"
+          price="3.49" />
+        <Roll 
+          imageURL={chocolate} 
+          rollName="Double-chocolate cinammon roll"
+          price="3.99" />
+        <Roll 
+          imageURL={strawberry} 
+          rollName="Strawberry cinammon roll"
+          price="3.99" />
+      </div>
     </div>
   );
 }
