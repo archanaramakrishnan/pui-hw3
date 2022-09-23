3d0ac2f56c43dc18f750edbec33ed8d2c4dff9ed
Archana Ramakrishnan <archana2359@gmail.com>
2022-09-22 22:53:57 -0400

    Created HomePage component

--- src/App.js
+++ src/App.js
@@ -1,51 +1,9 @@
-// import logo from './logo.svg';
 import './App.css';
-import original from './products/original-cinnamon-roll.jpg';
-import apple from './products/apple-cinnamon-roll.jpg';
-import raisin from './products/raisin-cinnamon-roll.jpg';
-import walnut from './products/walnut-cinnamon-roll.jpg';
-import chocolate from './products/double-chocolate-cinnamon-roll.jpg';
-import strawberry from './products/strawberry-cinnamon-roll.jpg';
-import logo from './logo/logo-01.svg';
-
-import Roll from './Roll.js';
-import NavBar from './NavBar';
+import HomePage from './views/home';
 
 function App() {
   return (
-    <div className="App">
-      <NavBar></NavBar>
-
-      <div className="list">
-        <Roll 
-          imageURL={original} 
-          rollName="Original cinammon roll"
-          price="2.49" />
-        <Roll 
-          imageURL={apple} 
-          rollName="Apple cinammon roll"
-          price="3.49" />
-        <Roll 
-          imageURL={raisin} 
-          rollName="Raisin cinammon roll"
-          price="2.99" />
-      </div>
-
-      <div className="list">
-        <Roll 
-          imageURL={walnut} 
-          rollName="Walnut cinammon roll"
-          price="3.49" />
-        <Roll 
-          imageURL={chocolate} 
-          rollName="Double-chocolate cinammon roll"
-          price="3.99" />
-        <Roll 
-          imageURL={strawberry} 
-          rollName="Strawberry cinammon roll"
-          price="3.99" />
-      </div>
-    </div>
+    <HomePage/>
   );
 }
 
